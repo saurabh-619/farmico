a934d8f4a50ecd83f67bf5831d8f39a6e46e0658
saurabh-619 <saurabhbomble107@gmail.com>
2022-04-01 17:59:24 +0530

    bugs: cookies bugs fixed

--- api/src/helpers/tokens.js
+++ api/src/helpers/tokens.js
@@ -95,12 +95,12 @@ exports.verifyRefreshToken = async (refreshToken) => {
 
 exports.setCookies = async (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
-    secure: !__dev__,
+    // secure: !__dev__,
     httpOnly: true,
     maxAge: ACCESS_TOKEN_EXPIRATION_COOKIE,
   });
   res.cookie("refreshToken", refreshToken, {
-    secure: !__dev__,
+    // secure: !__dev__,
     httpOnly: true,
     maxAge: REFRESH_TOKEN_EXPIRATION_REDIS_AND_COOKIE,
   });
@@ -108,12 +108,12 @@ exports.setCookies = async (res, accessToken, refreshToken) => {
 
 exports.destroyCookies = async (res) => {
   res.cookie("accessToken", "", {
-    secure: !__dev__,
+    // secure: !__dev__,
     httpOnly: true,
     maxAge: 0,
   });
   res.cookie("refreshToken", "", {
-    secure: !__dev__,
+    // secure: !__dev__,
     httpOnly: true,
     maxAge: 0,
   });
